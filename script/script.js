ad9560c39f74f3a91810316588751d2407a9509b
timnorov <timur.norov@gmail.com>
2021-05-15 14:06:16 +0300

    Fixed popup

--- script/script.js
+++ script/script.js
@@ -14,11 +14,47 @@ document.addEventListener('DOMContentLoaded', () => {
   
     clubSelect.addEventListener('click', (event) => {
       let target = event.target;
-      
+
       if (target.classList.contains('choose-club')) {
       clubsShow.classList.toggle('hidden-block');
       }
 
     });
+
+  //открываем поп-ап окна
+
+  const showPopup = document.querySelector('.open-popup'),
+        freeVisit = document.getElementById('free_visit_form'),
+        callBackBtn = document.querySelector('.callback-btn'),
+        callBack = document.getElementById('callback_form'),
+        fixedGift = document.querySelector('.fixed-gift'),
+        gift = document.getElementById('gift');
+
+  showPopup.addEventListener('click', () => {
+    freeVisit.style.display = 'block';
+  });
+
+  callBackBtn.addEventListener('click', () => {
+    callBack.style.display = 'block';
+  });
+
+  fixedGift.addEventListener('click', () => {
+    gift.style.display = 'block';
+    fixedGift.classList.add('hidden-block');
+  });
+
+  
+  //закрываем поп-ап записаться на визит
+  const body = document.querySelector('body');
+
+  body.addEventListener('click', (event) => {
+    let target = event.target;
+    console.log(target);
+    if (target.classList.contains('overlay') || target.classList.contains('close_icon') || target.classList.contains('close-btn')) {
+      freeVisit.style.display = 'none';
+      callBack.style.display = 'none';
+      gift.style.display = 'none';
+    }
+  })
     
 });
