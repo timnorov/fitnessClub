7ab76624d317f7d908afce201a1f542103cd5d81
timnorov <timur.norov@gmail.com>
2021-05-16 10:11:14 +0300

    Sticky menu

--- script/script.js
+++ script/script.js
@@ -337,45 +337,39 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     //фиксация бургер меню
-    // window.addEventListener('resize', function(event) {
-    // if (window.screen.width < 768) {
-    //   (function(){  
-    // var a = document.querySelector('.top-menu'), b = null;
-    // window.addEventListener('scroll', Ascroll, false);
+    (function(){  
+    let a = document.querySelector('.top-menu'), b = null;
+    window.addEventListener('scroll', Ascroll, false);
     
-    // function Ascroll() {
-    // if (b == null) { 
-    // var Sa = getComputedStyle(a, ''), s = '';
-    // for (var i = 0; i < Sa.length; i++) { 
-    //   if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-    //     s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-    //   }
-    // }
-    // b = document.createElement('div'); 
-    // b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-    // a.insertBefore(b, a.firstChild); 
-    // var l = a.childNodes.length;
-    // for (var i = 1; i < l; i++) {
-    //   b.appendChild(a.childNodes[1]);
-    // }
-    // a.style.height = b.getBoundingClientRect().height + 'px';
-    // a.style.padding = '0';
-    // a.style.border = '0'; 
-    // }
-    // if (a.getBoundingClientRect().top <= 0) { 
-    //   b.className = 'sticky';
-    // } else {
-    //   b.className = '';
-    // }
-    // window.addEventListener('resize', function() {
-    //   a.children[0].style.width = getComputedStyle(a, '').width
-    // }, false); 
-    // }
-    // })()
-
-    // } 
-    // }, true);
-    
+    function Ascroll() {
+    if (b == null) { 
+    let Sa = getComputedStyle(a, ''), s = '';
+    for (let i = 0; i < Sa.length; i++) { 
+      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+      }
+    }
+    b = document.createElement('div'); 
+    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+    a.insertBefore(b, a.firstChild); 
+    let l = a.childNodes.length;
+    for (let i = 1; i < l; i++) {
+      b.appendChild(a.childNodes[1]);
+    }
+    a.style.height = b.getBoundingClientRect().height + 'px';
+    a.style.padding = '0';
+    a.style.border = '0'; 
+    }
+    if (a.getBoundingClientRect().top <= 0 && window.screen.width < 768) { 
+      b.className = 'sticky';
+    } else {
+      b.className = '';
+    }
+    window.addEventListener('resize', function() {
+      a.children[0].style.width = getComputedStyle(a, '').width
+    }, false); 
+    }
+    })()
 
     
 });
